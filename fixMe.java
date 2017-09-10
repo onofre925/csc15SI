eef355eaaae983649c8bde333bcddf2ead904304
Emanuel Onofre <emanuelonofre@csus.edu>
2017-09-09 22:03:04 -0700

    Update fixMe.java

--- fixMe.java
+++ fixMe.java
@@ -1,16 +1,19 @@
-    public class fixMe
-{public static void main (String[] args){
-                  System.out.println("There's something wrong with this");
-  System.out.println("Can you fix me?");
-        NextMethod();
+    public class FixMe
+    {
+           public static void main (String[] args)
+                {
+                    System.out.println("There's something wrong with this");
+                    System.out.println("Can you fix me?");
+                     NextMethod();
+        
+                 }
+                    public static void NextMethod()
+                    {
+                           System.out.println("Are you annoyed yet?");
+                            OneMore();
                     }
-                        public static void NextMethod()
-{
-        System.out.println("Are you annoyed yet?");
-OneMore();
-                                                        }
-                                                        public static void OneMore()
-{
-                    System.out.println("You can see how this could be frustrating, right?");
-}
-                                      }
+                             public static void OneMore()
+                             {
+                              System.out.println("You can see how this could be frustrating, right?");
+                              }
+     }
